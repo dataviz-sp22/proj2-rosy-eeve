61012816daecb18b8e455bd784c9a9ece04d498e
Thao Tran <40745146+thaophuongtran@users.noreply.github.com>
2022-05-23 16:23:13 -0500

    Add files via upload
    311 data cleaning

---
+++ Data/Chicago_311_clean_merge.R
@@ -0,0 +1,35 @@
+#load library
+library(tidyverse)
+library(reshape2)
+
+#load data
+#311 data
+#https://data.cityofchicago.org/Service-Requests/311-Service-Requests/v6vf-nfxy
+df <- read.csv("C:/Users/bebut/Downloads/311_Service_Requests.csv")
+
+#acs data 
+acs = read.csv("data/Chicago_zcta_subset_acs2019_clean.csv")
+
+#311 data cleaning
+df = df %>%
+  mutate(
+    YEAR = as.numeric(substr(CREATED_DATE,7,10)),
+    ZIP = as.numeric(ZIP_CODE)
+  ) %>% 
+  filter(STATUS == "Completed" &
+           !is.na(ZIP) &
+           !is.na(LATITUDE) &
+           !is.na(LONGITUDE) &
+           YEAR %in% 2019
+  ) %>% select(
+    SR_NUMBER,SR_TYPE, OWNER_DEPARTMENT,CREATED_DATE,CLOSED_DATE,ZIP,YEAR,
+    CREATED_HOUR,CREATED_DAY_OF_WEEK,CREATED_MONTH
+    #X_COORDINATE,Y_COORDINATE,LATITUDE,LONGITUDE
+  )
+
+#merge 311 data and acs data 
+dfacs = df %>% left_join(acs,by=c("ZIP"="GEOID")) %>% na.omit()
+ 
+#write save data 
+save(dfacs,file = "data/Chicago_311_clean_merge.Rdata")
+
