236de6a4c5e201726732e70f822fb8352bf04d39
kamranahmed08 <kamranahmed@uchicago.edu>
2022-05-30 04:52:46 -0500

    Write up added to the app.R file

--- App/app.R
+++ App/app.R
@@ -327,8 +327,31 @@ server <- function(input, output) {
                    p("For our analysis, we use the data on 311 Service Requests received by the City of Chicago that are publicly available on the Chicago Data Portal. The dataset includes requests created after the launch of the new 311 system on 12/18/2018 and was last updated on May 11, 2022. Currently, it has 6 million rows and 37 columns, where each row is a request. Useful features from the data include request type, owner department, create date, closed date, and zip code. Since we are interested in the response time, we restrict observations to requests that have been completed. It is noted that the address for requests of the type “311 INFORMATION ONLY CALL” is often the address of the City’s 311 Center. See the codebook in data folder for all variables of interest."),
                    br(),
                    p("For demographic characteristics, we use data from The American Community Survey (ACS) that is a questionnaire conducted by the United States Census Bureau yearly to collect information about American citizens. Relevant sociodemographic elements were selected from this survey in the year 2019, and converted into a workable dataset using tidyCensus. Our subset of data includes 24 columns and 296 rows correlating to Chicago and other outlying areas included in the Chicago Metropolitan Statistical Area (MSA). We limit the data to observations with zip codes that match with the zip codes included in Chicago 311 dataset. Among the selected variables include factors relevant to race, education, age, gender, and socioeconomic status. While the already selected variables are robust, the many questions included in the original survey allow for us to add or reduce the number of variables included as necessary. See the codebook in the data folder for all variables of interest."),
-                  
-                   
+                   h2("Approach:"),
+                   br(),
+                   p("We believe that spatial visualizations are best suited to examine volume of different 311 service request types, response time to address those requests and varying sociodemographic characteristics across different areas of Chicago. To aid in illustrating the characteristics of a selected service domain, we use additional basic ggplot2 visualizations i.e., bar charts to show the distribution of different request types within a domain/department for the top and bottom quartile of the selected demographic group. Switching to a different demographic group updates these bar charts along with the choropleth for the share of the selected sociodemographic group in different areas."),
+                   br(),
+                   p("This helps to show the relation between the department with initial responsibility for the service request or service type and socioeconomic indicators. For example, there is a positive correlation between influx of a DOB – Building service request and the share of native population. The bar charts help look at this relation in a more clear way as we are able to see the volume and request time for each request type for both the top and bottom quartile of selected characteristic. The comparison becomes much easier and detailed. "),
+                   br(),
+                   p("We add an element of interactivity to this analysis using Shiny app. The app provides the feature to toogle certain categories of service requests, as well as select a community area in order to see the distribution of service requests and the demographic patterns associated with the frequency of requests in the area."),
+                   br(),
+                   h2("Discussion:"),
+                   br(),
+                   p("The choropleths of population share for socio-demographic characteristics show that the northern communities are concentrated with white population whereas the southern communities/localities with black population. Moreover, natives seem to be fairly evenly distributed with a relatively slightly higher concentration in the eastern lakeside areas. Asians and Native Hawaiian and Pacific Islanders are relatively densely populated in the north than the south. Hispanic are relatively densely populated in the western communities than the east. The eastern communities also seem to have a relatively higher GINI than the west. Median income is relatively higher for the north parts of Chicago than the south. Income through public assistance is however higher for the south whereas income with no public assistance is higher for the north. Education level is higher for the north and lower for the south. Male population is slightly higher in the north and female in south. Youth population is higher in the west and aged 18-34 in the east around downtown and commercial areas. "),
+                   br(),
+                   p("Request for City Services seem to come from a community with higher black population share, higher GINI and higher income with public assistance."),
+                   br(),
+                   p("Requests related to animal care and control seem to come more from communities with higher black population share, and low media income and lower education level."),
+                   br(),
+                   p("Request for Services related to aviation come solely from region with high white population share, and high income with no public assistance."),
+                   br(),
+                   p("Request for Services related to Business Affairs and Consumer Protection come from communities with higher white population share, higher Native Hawaiian and Pacific Islanders population share and higher level of education."),
+                   br(),
+                   p("Request for Services related to planning and building come more from region with high Hispanic population share, and high income with no public assistance and lower education level."),
+                   br(),
+                   p("Request for Services related to planning and building come more from region with high black and Hispanic population share, high GINI, and lower median income and lower education level."),
+                   br(),
+                   p("Request for Services related to water and management come more from communities with high Hispanic population share, lower median income and lower education level."),
         ))
     })
     
